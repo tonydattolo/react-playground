f9ff7d9b10bbe60a16ed0ad7603754f1c16baa06
tonydattolo <tdattolo@iu.edu>
2021-04-11 18:50:24 -0400

    voting app complete

--- fullstack-react-book/voting_app/public/js/app-2.js
+++ fullstack-react-book/voting_app/public/js/app-2.js
@@ -1,17 +1,49 @@
-
 class ProductList extends React.Component {
+  // state is owned by the component. when state or props updates,
+  // the component re-renders
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     products: [], // component mounts with empty products array
+  //   };
+  //   // bind the custom method, so it references this component
+  //   this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  // }
+  state = {
+    products: [], // component mounts with empty products array
+  };
+
+  // state changes must take place using this.setState()
+  componentDidMount() {
+    // after loading, state.products is updated with data, and re-renders
+    this.setState({ products: Seed.products });
+  }
+
   // children communicate events with their parent component
   // through functions that are passed down
-  handleProductUpVote(productId) {
-    console.log(productId + ' was upvoted.')
-  };
+  handleProductUpVote = (productId) => {
+    const nextProducts = this.state.products.map((product) => {
+      if (product.id === productId) {
+        return Object.assign({}, product, { // .assign clones product to a new {} with vote updated
+          votes: product.votes + 1,
+        });
+      } else {
+        // if product id doesnt match, we return it unmodified
+        return product;
+      }
+    });
+
+    this.setState({
+      products: nextProducts,
+    });
+  }
+  
   render() {
-    const products = Seed.products.sort((a,b) => (
-      b.votes - a.votes
-    ));
+    const products = this.state.products.sort((a, b) => b.votes - a.votes);
     const productComponents = products.map((product) => (
-      <Product 
-        id={'product-' + product.id}
+      <Product
+        key={"product-" + product.id} // DONT FORGET UNIQUE KEY
+        id={product.id}
         title={product.title}
         description={product.description}
         url={product.url}
@@ -21,11 +53,7 @@ class ProductList extends React.Component {
         onVote={this.handleProductUpVote}
       />
     ));
-    return (
-      <div className='ui unstackable items'>
-        {productComponents}
-      </div>
-    );
+    return <div className="ui unstackable items">{productComponents}</div>;
   }
 }
 
@@ -34,43 +62,39 @@ class Product extends React.Component {
   //       such as render() or componentDidMount()
   //       when defining custom methods, manual binding is required.
   //       this is done using constructor method
-  constructor(props) {
-    super(props);
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleUpVote = this.handleUpVote.bind(this);
+  // }
 
   // this.props.onVote was passed down from the parent
   // we can now call this childs function, which will call the
   // onVote function prop that was passed down, which calls a function
-  handleUpVote() {
+  handleUpVote = () => (
     this.props.onVote(this.props.id)
-  }
+  );
 
   render() {
     return (
-      <div className='item'>
-        <div className='image'>
+      <div className="item">
+        <div className="image">
           <img src={this.props.productImageUrl} />
         </div>
-        <div className='middle aligned content'>
-          <div className='header'>
+        <div className="middle aligned content">
+          <div className="header">
             <a onClick={this.handleUpVote}>
-              <i className='large caret up icon' />
+              <i className="large caret up icon" />
             </a>
             {this.props.votes}
           </div>
-          <div className='description'>
-            <a href={this.props.url}>
-              {this.props.title}
-            </a>
-            <p>
-              {this.props.description}
-            </p>
+          <div className="description">
+            <a href={this.props.url}>{this.props.title}</a>
+            <p>{this.props.description}</p>
           </div>
-          <div className='extra'>
+          <div className="extra">
             <span>Submitted by:</span>
             <img
-              className='ui avatar image'
+              className="ui avatar image"
               src={this.props.submitterAvatarUrl}
             />
           </div>
@@ -80,7 +104,4 @@ class Product extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <ProductList />,
-  document.getElementById('content')
-);
+ReactDOM.render(<ProductList />, document.getElementById("content"));
